6dcd86ca4196cc4505017dfc56b23934dbc7e82e
Italo Costa <italo.costa99@gmail.com>
2024-10-05 16:32:38 -0300

    feat(): index.php created to deal with api request

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$routes = require_once __DIR__ . '/../routes/api.php';
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+$uri = trim($uri, '/');
+
+function matchRoute( $route, $uri) {
+    $route = trim($route, '/');
+    $routeParts = explode('/', $route);
+    $uriParts = explode('/', $uri);
+
+    if (count($routeParts) !== count($uriParts)) {
+        return false;
+    }
+
+    $params = [];
+    foreach ($routeParts as $key => $part) {
+
+        if (strpos($part, '{') === 0 && strpos($part, '}') === strlen($part) - 1) {
+            $paramName = trim($part, '{}');
+            $params[$paramName] = $uriParts[$key];
+        } elseif ($routeParts[$key] !== $uriParts[$key]) {
+            return false;
+        }
+    }
+
+    return $params;
+}
+
+
+if (isset($routes[$method])) {
+    $matched = false;
+
+    foreach ($routes[$method] as $route => $handler) {
+
+        $params = matchRoute($route, $uri);
+
+        if ($params !== false) {
+            [$controller, $method] = $handler;
+
+            if (class_exists($controller) && method_exists($controller, $method)) {
+                $instance = new $controller();
+                $instance->$method(...$params);
+                $matched = true;
+                break;
+            }
+        }
+    }
+
+    if (!$matched) {
+        http_response_code(404);
+        echo json_encode(['error' => 'Route not found']);
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+}
